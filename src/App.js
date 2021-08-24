bf7b968e1816f4e788d39c22ff274e3a6a135b49
asmisomani <asmi.somani@gmail.com>
2021-08-24 18:40:04 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Landing from "./pages/Landing";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./stylesheets/bootstrap_change.css"
+import "./stylesheets/bootstrap_change.css";
+import "./stylesheets/material-ui.css"
 import {Route , Switch} from "react-router-dom"; 
 import UserGuide from "./pages/UserGuide";
 import SetPlan from "./pages/SetPlan"
+import Navbar from "./pages/Navbar";
 
 function App() {
   return (
@@ -15,6 +17,11 @@ function App() {
       <Route path="/setplan">
         <SetPlan/>
       </Route>
+      <Switch>
+        <Route path="/home">
+          <Navbar/>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MainNav from "../components/MainNav"
+import "../stylesheets/navbar.css"
+
+export default function Navbar() {
+    return (
+        <div>
+            <MainNav/>
+        </div>
+    )
+}
